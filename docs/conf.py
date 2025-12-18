903f9f8f580078c7b5203d09e1b807c7df3d8fa3
MichalWierzbicki886 <michal.wierzbicki@tlen.pl>
2025-12-18 17:47:19 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# conf.py
+project = 'Moja Dokumentacja'
+author = 'Jan Kowalski'
+release = '1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
